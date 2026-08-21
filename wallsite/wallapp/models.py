3dcfbf3d960bf5985ce0ba7b2cea31c871058a11
SENMART28 <ttnmrtnk@gmail.com>
2026-08-21 17:17:40 +0300

    Исправил ошибку с отображением автора

--- wallsite/wallapp/models.py
+++ wallsite/wallapp/models.py
@@ -7,7 +7,7 @@ class Wall(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок поста')
     slug = AutoSlugField(populate_from='title', unique=True)
     text = models.TextField(max_length=500, verbose_name='Текст поста')
-    private = models.BooleanField(verbose_name='Отображение автора')
+    private = models.BooleanField(verbose_name='Опубликовать анонимно')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время последнего обновления')
     author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, default=None, blank=True, null=True, verbose_name='Автор')
