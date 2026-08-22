8f196e7c79e201ac1814001ef955c1b73cbcbd3a
SENMART28 <ttnmrtnk@gmail.com>
2026-08-22 17:23:07 +0300

    Добавил систему лайков у постов

--- wallsite/wallapp/models.py
+++ wallsite/wallapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
+
 class Wall(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок поста')
     slug = AutoSlugField(populate_from='title', unique=True)
@@ -11,7 +12,7 @@ class Wall(models.Model):
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время последнего обновления')
     author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, default=None, blank=True, null=True, verbose_name='Автор')
-    likes = models.ManyToManyField(get_user_model(), default=None, blank=True, verbose_name='Лайкнувшие пользователи', related_name='likes')
+    likes = models.ManyToManyField(get_user_model(), blank=True, verbose_name='Лайкнувшие пользователи', related_name='Лайки')
     
     def __str__(self):
         return self.title
