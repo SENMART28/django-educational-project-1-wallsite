604e7a918fff95ce2ed64cc9fb938b4ec6c330b3
SENMART28 <ttnmrtnk@gmail.com>
2026-09-20 16:06:00 +0300

    Полностью доделал фикс багов

--- wallsite/users/utils.py
+++ wallsite/users/utils.py
@@ -1,8 +1,8 @@
 class TitleMixin:
     title_page = None
     
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         if self.title_page:
             context['title_page'] = self.title_page
             
