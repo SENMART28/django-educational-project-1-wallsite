8cf8284eae524419968a486987201124674dd864
SENMART28 <ttnmrtnk@gmail.com>
2026-09-20 16:32:49 +0300

    При помощи pico добавил стили на сайт. Добавил index_pico_with_ai.html где находятся улучшенные ИИ стили pico в которых я пока не разобрался;)

--- wallsite/wallsite/settings.py
+++ wallsite/wallsite/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 AUTH_USER_MODEL = 'users.User'
 
